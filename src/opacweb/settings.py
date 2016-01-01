aefc6ea8fedadc5e0cdeb9d6e01327851f6ed018
Raphael Michel <mail@raphaelmichel.de>
2016-01-01 13:45:08 +0100

    Installed django-cms

--- src/opacweb/settings.py
+++ src/opacweb/settings.py
@@ -6,25 +6,39 @@ SECRET_KEY = '%79v02bff$dbio6kld*$a-!pvis*+1y6o!=kl4d-^kf6+lwl*g'
 DEBUG = True
 ALLOWED_HOSTS = []
 
-
 INSTALLED_APPS = [
+    'djangocms_admin_style',  # for the admin skin.
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'cms',  # django CMS itself
+    'treebeard',  # utilities for implementing a tree
+    'menus',  # helper for model independent hierarchical website navigation
+    'sekizai',  # for JavaScript and CSS management
+    'reversion'
 ]
 
 MIDDLEWARE_CLASSES = [
+    'cms.middleware.utils.ApphookReloadMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'cms.middleware.user.CurrentUserMiddleware',
+    'cms.middleware.page.CurrentPageMiddleware',
+    'cms.middleware.toolbar.ToolbarMiddleware',
+    'cms.middleware.language.LanguageCookieMiddleware',
 ]
 
 ROOT_URLCONF = 'opacweb.urls'
@@ -32,7 +46,9 @@ ROOT_URLCONF = 'opacweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'opacweb/templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -40,6 +56,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'sekizai.context_processors.sekizai',
+                'cms.context_processors.cms_settings',
             ],
         },
     },
@@ -54,19 +72,24 @@ DATABASES = {
     }
 }
 
-
-AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'}
-]
-
+SITE_ID = 1
 
 LANGUAGE_CODE = 'en-us'
+LANGUAGES = (
+    ('de', 'Deutsch'),
+    ('en-us', 'English')
+)
 TIME_ZONE = 'Europe/Berlin'
 USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CMS_TEMPLATES = (
+    ('opacappnet/template_main.html', 'Main Template'),
+)
--- src/opacweb/urls.py
+++ src/opacweb/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.i18n import i18n_patterns
+from django.conf.urls.static import static
 from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = i18n_patterns('',
     url(r'^admin/', admin.site.urls),
-]
+    url(r'^', include('cms.urls')),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
